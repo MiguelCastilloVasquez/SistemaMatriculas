9adb6fc415d6597217069355ca5ba7abfbba76ad
lgcid <lgcid@alumnos.ubiobio.cl>
2016-07-25 10:30:54 -0300

    Index creado

--- index.php
+++ index.php
@@ -8,10 +8,21 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title></title>
+        <link rel="stylesheet" href="css/materialize.min.css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <nav>
+            <div class="nav-wrapper fixed green darken-4">
+                <a href="index.php" class="brand-logo"><img src="img/adventista.png" style="height: 9%; width: 9%;"></a>
+                <ul class="right hide-on-med-and-down">
+                    <li><a href="login.php"><i class="material-icons left">people</i>Profesores</a></li>
+                    <li><a href="login_2.php"><i class="material-icons left">person</i>Administrador</a></li>
+                </ul>
+            </div>
+        </nav>
+        <!-- Adjuntando los archivos JS -->
+        <script src="js/jquery.min.js"></script>
+        <script src="js/materialize.min.js"></script>
     </body>
 </html>
