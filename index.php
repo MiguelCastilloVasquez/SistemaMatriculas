88b53f9580b9840b9bf43dc9645ab03e1bacbad5
MiguelCastilloVasquez <m.castillo.vas@gmail.com>
2016-07-25 11:48:20 -0400

    Separacion Pagina Inicio

--- index.php
+++ index.php
@@ -14,13 +14,33 @@ and open the template in the editor.
     <body>
         <nav>
             <div class="nav-wrapper fixed green darken-4">
-                <a href="index.php" class="brand-logo"><img src="img/adventista.png" style="height: 9%; width: 9%;"></a>
+                <a class="brand-logo"><img src="img/adventista.png" style="height: 9%; width: 9%;"></a>
                 <ul class="right hide-on-med-and-down">
                     <li><a href="login_profesores.php"><i class="material-icons left">people</i>Profesores</a></li>
                     <li><a href="#"><i class="material-icons left">person</i>Administrador</a></li>
                 </ul>
             </div>
         </nav>
+
+        <div class="row" >
+            <div class="col s3" style="background: red">
+                <div>
+                    Campo 1
+                    <a href="#">CAS</a>
+                </div>                
+            </div>
+
+            <div class="col s6" style="background: yellowgreen">
+                <div>
+                    Campo 2
+                </div>
+            </div>
+
+            <div class="col s3" style="background: blue">
+                <h5 class="center-align">Imagenes</h5>
+            </div>
+        </div>
+
         <!-- Adjuntando los archivos JS -->
         <script src="js/jquery.min.js"></script>
         <script src="js/materialize.min.js"></script>
