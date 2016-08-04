667b661b299752f39d6ccd2c342140d125e15079
MiguelCastilloVasquez <m.castillo.vas@gmail.com>
2016-08-04 09:54:59 -0400

    Solucion tildes base datos

--- conexion_bd.php
+++ conexion_bd.php
@@ -1,6 +1,6 @@
 <?php
     try{
-        $dataBase = new PDO("mysql:host=localhost; dbname=adventista", "root", "");
+        $dataBase = new PDO("mysql:host=localhost; dbname=adventista", "root", "", array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES  \'UTF8\''));
         $dataBase->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (Exception $ex) {
         die("Error: " . $ex->getMessage());
